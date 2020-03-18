882eb9b857c7fee6fe25a04a463b89a4d2d84186
Emadmenkar2018 <34415236+Emadmenkar2018@users.noreply.github.com>
2020-03-18 16:32:15 +0300

    kilo circle shape fixed

--- src/screens/banaOzel/Goals/KiloGoalScreen.js
+++ src/screens/banaOzel/Goals/KiloGoalScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet,   View,Text ,Dimensions} from 'react-native';    
+import { StyleSheet,   View,Text ,Dimensions,TouchableHighlight} from 'react-native';    
 import  MyPieChart  from '../../../../components/Banaozel/Goals/MyPieChart'
 
   const screenWidth = Dimensions.get("window").width;
@@ -13,13 +13,30 @@ import  MyPieChart  from '../../../../components/Banaozel/Goals/MyPieChart'
             <View style={{backgroundColor:'#fff',zIndex:1,width:'100%',height:'100%'}}> 
 
                 <View style={{alignSelf:'center',backgroundColor:'transparent',alignItems:'center',alignContent:'center' }}>
-                    
+                
+                  <View style={{flexDirection:'row',alignItems:'center',justifyContent:'center',width:'100%'}}>
+                    <Text style={{marginRight:20,fontFamily:'Muli-Bold',fontWeight:'600',fontSize:18}}>Kilo Hedefi</Text>
+                    <TouchableHighlight
+                      style = {styles.circle1}
+                      underlayColor = '#ccc'   >
+                      <Text>{data.length >0   ?  data[0].content :''} Kg</Text>
+                    </TouchableHighlight>
+                  </View>
 
-                    <MyPieChart
+
+                  <View style={{flexDirection:'row',alignItems:'center',justifyContent:'center',width:'100%'}}>
+                    <Text style={{marginRight:20,fontFamily:'Muli-Bold',fontWeight:'600',fontSize:18}}>Kilonuz</Text>
+                    <TouchableHighlight
+                      style = { styles.circle2}
+                      underlayColor = '#ccc'   >
+                      <Text> {current  ? current.content : ''} Kg</Text>
+                    </TouchableHighlight>
+                  </View>
+                    {/* <MyPieChart
                       title={'Kilo'}
                       data={data.length >0   ?  data[0].content :''}
                       currentData= {current  ? current.content : ''}
-                    />
+                    /> */}
 
                     <Text style={{marginVertical:5}}>Not: You have to work hard </Text>
 
@@ -35,5 +52,26 @@ import  MyPieChart  from '../../../../components/Banaozel/Goals/MyPieChart'
 export default KiloGoalScreen;
 
 const styles = StyleSheet.create({ 
-
+  circle1:{
+    borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
+    width: Dimensions.get('window').width * 0.35,
+    height: Dimensions.get('window').width * 0.35,
+    backgroundColor:'#fff',
+    borderWidth:2,
+    borderColor:'#4d961d',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical:6
+  },
+  circle2:{
+    borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
+    width: Dimensions.get('window').width * 0.35,
+    height: Dimensions.get('window').width * 0.35,
+    backgroundColor:'#fff',
+    borderWidth:2,
+    borderColor:'#faa739',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical:6
+  }
   });
